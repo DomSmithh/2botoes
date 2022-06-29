8b288c24ba4ca783aded0d57d758c953deb64768
DomSmithh <douglas.gon22222@otmail.com>
2022-06-29 20:45:17 -0300

    Alterando src para atividade

--- src/App.jsx
+++ src/App.jsx
@@ -1,43 +1,29 @@
-import Title from './Title';
-import Noticia from './Noticia';
-import "./App.css"
+import Button from "./Button";
 
-function App() {
-    const lista_noticias = [
-        {
-            numeral: "1",
-            description: "Funcionária que denunciou procurador diz que agressor 'desprezava mulheres' e teme sair na rua:'Muito medo'"
-        },        
-        {
-            numeral: "2",
-            description: "Imposto de Renda 2022: Receita abre consultas ao 2° lote de restituição nesta quinta; veja se você está no grupo"
-        },
-        {
-            numeral: "3",
-            description: "Governo opera para evitar CPI do MEC, que ganhou força após prisão de Milton Ribeiro"
-        },
-        {
-            numeral: "4",
-            description: "Em Itajubá, assaltantes armados etacam agência da Caixa Econômica Federal; 7 ficam feridos"
-        },
-        {
-            numeral: "5",
-            description: "Almir Sater é 'parado em blitz' e policiais tietam cantor: 'o senhor é o dono dessa chalana?'"
-        }
-    ];
+function App(){
+  const botao = [
+    {
+      tit: "PREVIOUS",
+      sub: "JQUERY: DETECT UPDATE OF HTML ELEMENT",
+    },
+    {
+      tit2: "NEXT",
+      sub2: "RAILS + DEVISE WORKING WITH MULTIPLE MODELS"
+    },
+  ];
 
-    return (
-        <div className='container'>
-            <Title />
-
-            {lista_noticias.map( (noticia) => {
-                return <Noticia
-                num={noticia.numeral} desc={noticia.description} />
-            })}
-
-        </div>
-
-    );
+  return (
+    <div>
+      {botao.map((boton) => {
+        return <Button key={boton}
+                tit={boton.tit}
+                sub={boton.sub}
+                tit2={boton.tit2}
+                sub2={boton.sub2}
+                />;
+      })
+      }
+    </div>
+  )
 }
-
-export default App
+export default App;
