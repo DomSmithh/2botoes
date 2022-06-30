34c64987c005996a59668bc486a67663fd2bb728
DomSmithh <douglas.gon22222@otmail.com>
2022-06-29 21:10:42 -0300

    Alterando Button e App

--- src/App.jsx
+++ src/App.jsx
@@ -3,26 +3,25 @@ import Button from "./Button";
 function App(){
   const botao = [
     {
-      tit: "PREVIOUS",
-      sub: "JQUERY: DETECT UPDATE OF HTML ELEMENT",
+      title1: "PREVIOUS",
+      sub1: "JQUERY: DETECT UPDATE OF HTML ELEMENT",
     },
     {
-      tit2: "NEXT",
+      title2: "NEXT",
       sub2: "RAILS + DEVISE WORKING WITH MULTIPLE MODELS"
     },
   ];
 
   return (
     <div>
-      {botao.map((boton) => {
-        return <Button key={boton}
-                tit={boton.tit}
-                sub={boton.sub}
-                tit2={boton.tit2}
-                sub2={boton.sub2}
+      {botao.map((button) => {
+        return <Button key={button}
+                title1={button.title1}
+                sub1={button.sub1}
+                title2={button.title2}
+                sub2={button.sub2}
                 />;
-      })
-      }
+      }) };
     </div>
   )
 }
--- src/Button.jsx
+++ src/Button.jsx
@@ -1,16 +1,16 @@
 import "./Button.css";
 
-function Button({tit, sub, tit2, sub2}){
+function Button({title1, sub1, title2, sub2}){
     return(
 
         <button className="container">
-            <div className="primeiro">
-            <strong><div className="titulo">{tit}</div></strong>
-            <div className="subtitulo">{sub}</div>
+            <div className="button-um">
+            <strong><div className="titulo1">{title1}</div></strong>
+            <div className="subtitulo1">{sub1}</div>
             </div>
         
-            <div className="segundo">
-            <strong><div className="titulo2">{tit2}</div></strong>
+            <div className="button-dois">
+            <strong><div className="titulo2">{title2}</div></strong>
             <div className="subtitulo2">{sub2}</div>
             </div>
         </button>
